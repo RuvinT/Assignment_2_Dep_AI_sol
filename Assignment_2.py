380f73104d812b7e30652da8cb134232a4cdf7cb
RuvinT <jagodaruvin@gmail.com>
2024-02-02 12:54:34 -0500

    Add new file Assignment_2.py

---
+++ Assignment_2.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb  2 12:44:23 2024
+
+@author: ruvinjagoda
+"""
+
+print ("Hello world Deployment of AI Solutions 02 (M07 Group 2)")
