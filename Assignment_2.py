1ef29f808747eadc3eafd4d5114d8b2dca56549e
RuvinT <jagodaruvin@gmail.com>
2024-02-06 20:39:35 -0500

    test case

--- Assignment_2.py
+++ Assignment_2.py
@@ -8,3 +8,8 @@ Created on Fri Feb  2 12:44:23 2024
 
 print ("Hello world Deployment of AI Solutions 02 (M07 Group 2)")
 print ("sample branch update")
+
+# In test_example.py
+
+def test_addition():
+    assert 2 + 2 == 4
